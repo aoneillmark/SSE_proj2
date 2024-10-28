5dbb90aeea5549c0481a71670a017e6dfbc84600
Mark O'Neill <mo1024@imperial.ac.uk>
2024-10-28 16:28:59 +0000

    End of assignment 3

--- app.py
+++ app.py
@@ -14,3 +14,14 @@ def submit():
     input_message = request.form.get("message")
     return render_template("hello.html",
                            email=input_email, message=input_message)
+
+@app.route("/query")
+def get_query():
+    query = request.args.get('q')
+    return process_query(query)
+
+def process_query(animal):
+    if animal == "dinosaurs":
+        return "Dinosaurs ruled the Earth 200 million years ago"
+    else: 
+        return "Unknown"
