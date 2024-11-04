fce82cb7ee29f1bddb7d8a98d5b9925ce1ae7e0f
Mark O'Neill <aoneillmark@gmail.com>
2024-11-04 14:45:00 +0000

    Remove dinosaur check

--- app_test.py
+++ app_test.py
@@ -3,9 +3,9 @@ from app import process_query
 fact = "Dinosaurs ruled the Earth 200 million years ago"
 
 
-def test_knows_about_dinosaurs():
-    assert process_query("dinosaurs") == fact
+# def test_knows_about_dinosaurs():
+#     assert process_query("dinosaurs") == fact
 
 
-def test_does_not_know_about_asteroids():
-    assert process_query("asteroids") == "Unknown"
+# def test_does_not_know_about_asteroids():
+#     assert process_query("asteroids") == "Unknown"
